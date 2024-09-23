1e5bc5185dca1bc5ca3e83ed6cb7cc12de4ecd03
BenTroyCahoon <145575653+BenTroyCahoon@users.noreply.github.com>
2024-09-23 11:57:33 +0200

    Navigation (#7)
    * added navigation skeletom
    
    * changed folder name to capital letter

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,18 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./Styles/App.css";
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navigation from "./Navigation/Navigate";
 
-function App() {
-  const [count, setCount] = useState(0);
-
+const App: React.FC = () => {
   return (
-    <>
-      <div>
-        <h1>Glöm inte att ha roligt !</h1>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Router>
+      <Navigation />
+      <Routes>
+        <Route path="/" element={<h1>Home Page</h1>} />
+        <Route path="/trending" element={<h1>Trending</h1>} />
+        <Route path="/recommended" element={<h1>Recommended for You</h1>} />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
