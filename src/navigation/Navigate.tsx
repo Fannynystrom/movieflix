2191a52e5ee749d3e8e060361dff344c5f1f8855
Yxan <adamaxelsson99@hotmail.com>
2024-09-28 00:13:12 +0200

    Searchbar *OBS Stylea

--- src/navigation/Navigate.tsx
+++ src/navigation/Navigate.tsx
@@ -1,19 +1,26 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { FaBookmark } from "react-icons/fa";
-
+import useFetchMovies from "../hooks/FetchMovies";
+import SearchBar from "../components/SearchBar";
 import "../styles/Navigation.css";
 
 const Navigate: React.FC = () => {
+  const { movies } = useFetchMovies();
+
   return (
     <nav className="navbar">
+
       <Link to="/">Startsida</Link>
       <Link to="/categoriesscreen">Kategorier</Link>
       <Link to="/trendingcarousel">Trending</Link>
       <Link to="/recommendedcarousel">Recommended</Link>
-      <Link to="/bookmarked" className="bookmark-icon">
-        <FaBookmark size={24} />
-      </Link>
+      <div className="icons">
+        <SearchBar data={movies} />
+        <Link to="/bookmarked" className="bookmark-icon">
+          <FaBookmark size={24} />
+        </Link>
+      </div>
     </nav>
   );
 };
