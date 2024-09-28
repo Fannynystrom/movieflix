aa2fcd1f494610774f4c8ee8289714cbd09b7c67
Yxan <adamaxelsson99@hotmail.com>
2024-09-28 13:26:26 +0200

    Div för att skilja ikoner i på startsida + visa max 8 filmer i sök

--- src/navigation/Navigate.tsx
+++ src/navigation/Navigate.tsx
@@ -10,11 +10,12 @@ const Navigate: React.FC = () => {
 
   return (
     <nav className="navbar">
-
+      <div>
       <Link to="/">Startsida</Link>
       <Link to="/catogories">Kategorier</Link>
       <Link to="/trendingcarousel">Trending</Link>
       <Link to="/recommendedcarousel">Recommended</Link>
+      </div>
       <div className="icons">
         <SearchBar data={movies} />
         <Link to="/bookmarked" className="bookmark-icon">
