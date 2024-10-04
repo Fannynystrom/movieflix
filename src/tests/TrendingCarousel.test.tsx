1d5821caf711bfb679b9dc40beb6316c02151454
Fannynystrom <120643333+Fannynystrom@users.noreply.github.com>
2024-10-04 15:02:10 +0200

    prettier

--- src/tests/TrendingCarousel.test.tsx
+++ src/tests/TrendingCarousel.test.tsx
@@ -1,8 +1,8 @@
 import { render, screen } from "@testing-library/react";
 import { describe, it, expect, vi } from "vitest"; // Använd vitest för tester
-import TrendingCarousel from "../components/TrendingCarousel"; 
-import { Movie } from "../types/Movies"; 
-import { BookmarksProvider } from "../context/BookmarksContext"; 
+import TrendingCarousel from "../components/TrendingCarousel";
+import { Movie } from "../types/Movies";
+import { BookmarksProvider } from "../context/BookmarksContext";
 
 // Skapa mockdata för att simulera filmer
 const mockMovies: Movie[] = [
